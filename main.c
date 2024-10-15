e31ea29691698cea43d60e93349f3f40d426518e
Achille Bosc <abosc@c1r2p12.42lehavre.fr>
2024-10-15 21:27:02 +0200

    smal changes

--- main.c
+++ main.c
@@ -6,13 +6,16 @@
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:54:17 by abosc             #+#    #+#             */
-/*   Updated: 2024/10/15 18:32:30 by abosc            ###   ########.fr       */
+/*   Updated: 2024/10/15 20:29:58 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
+	char	str[] = "abcdef";
+	printf("%s", (char *)memset(str, 'X', 3));
 }
