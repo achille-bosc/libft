59a3d53022dd838ef75bc07658e2cd80f7ca737f
Achille BOSC <136481630+achille-bosc@users.noreply.github.com>
2024-11-17 21:44:49 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 23:57:23 by abosc             #+#    #+#             */
+/*   Updated: 2024/11/15 19:54:45 by abosc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "libft.h"
+
+static int	ft_gnl_endl(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	if (str[i] == '\n')
+		return (1);
+	return (0);
+}
+
+
+char	*ft_get_line(char *buffer)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	str = ft_substr(buffer, 0, i + ft_gnl_endl(buffer));
+	if (!str)
+	{
+		free(str);
+		return (NULL);
+	}
+	return (str);
+}
+
+char	*ft_new_str(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	str = malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
+	if (!str)
+	{
+		free(str);
+		return (NULL);
+	}
+	i++;
+	j = 0;
+	while (buffer[i])
+		str[j++] = buffer[i++];
+	str[j] = '\0';
+	free(buffer);
+	return (str);
+}
+
+char	*ft_read_str(int fd, char *buffer)
+{
+	char	*s;
+	int		bytes;
+
+	s = malloc((GNL_BUFFER_SIZE + 1) * sizeof(char));
+	if (!s)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(buffer, '\n') && bytes != 0)
+	{
+		bytes = read(fd, s, GNL_BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(s);
+			return (NULL);
+		}
+		s[bytes] = '\0';
+		buffer = ft_strjoin(buffer, s);
+	}
+	free(s);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*buffer;
+
+	if (fd < 0 || GNL_BUFFER_SIZE <= 0)
+		return (0);
+	buffer = ft_read_str(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_get_line(buffer);
+	buffer = ft_new_str(buffer);
+	return (line);
+}
